9dd2cef8ab25dfd5cf52e95e3a46e267ce7970b0
Yannick Van Campenhout <Yannick@cloudclass.be>
2021-12-19 18:40:52 +0100

    Color rework + positioning

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,30 +1,28 @@
 <template>
-    <div>
-        <h1>
-            Eurosong voting app!
-        </h1>
-        <div class="center">
-            <button class="cybr-btn" @click="goToPage('game')">
-                Check the votes<span aria-hidden>_</span>
-                <span aria-hidden class="cybr-btn__glitch">Check the votes</span>
-                <span aria-hidden class="cybr-btn__tag">#1</span>
-            </button>
-            <button class="cybr-btn" @click="goToPage('ranking')">
-                Check the ranking<span aria-hidden>_</span>
-                <span aria-hidden class="cybr-btn__glitch">Check the ranking</span>
-                <span aria-hidden class="cybr-btn__tag">#2</span>
-            </button>
-        </div>
+  <div>
+    <h1>Eurosong voting app!</h1>
+    <div class="center">
+      <button class="cybr-btn" @click="goToPage('ranking')">
+        Check the ranking<span aria-hidden>_</span>
+        <span aria-hidden class="cybr-btn__glitch">Check the ranking</span>
+        <span aria-hidden class="cybr-btn__tag">#2</span>
+      </button>
+      <button class="cybr-btn" @click="goToPage('game')">
+        Check the votes<span aria-hidden>_</span>
+        <span aria-hidden class="cybr-btn__glitch">Check the votes</span>
+        <span aria-hidden class="cybr-btn__tag">#1</span>
+      </button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Homepage",
-        methods: {
-            goToPage(page) {
-                this.$emit("change-page", page);
-            }
-        }
-    }
+export default {
+  name: "Homepage",
+  methods: {
+    goToPage(page) {
+      this.$emit("change-page", page);
+    },
+  },
+};
 </script>
--- src/pages/Rankingpage.vue
+++ src/pages/Rankingpage.vue
@@ -1,113 +1,123 @@
 <template>
-    <div>
-
-        <button class="cybr-btn" @click="goToPage('home')">
-        Go home<span aria-hidden>_</span>
-        <span aria-hidden class="cybr-btn__glitch">Go home</span>
-        <span aria-hidden class="cybr-btn__tag">#1</span>
-        </button>
-        <h1>
-            Ranking
-        </h1>
-        <table>
-            <tr>
-                <th>Ranking</th>
-                <th>Song Title</th>
-                <th>Song Artist</th>
-                <th>Total Points</th>
-                <th>Total %</th>
-                <th>Spotify Link</th>
-            </tr>
-            <tr v-for="(song, i) in songs" :key="i">
-                <td>#{{i + 1}}</td>
-                <td>{{song.title}}</td>
-                <th>{{song.artist}}</th>
-                <td>{{song.totalPoints}}</td>
-                <td>{{song.percentage}}%</td>
-                <td><iframe :src="song.spotify" height="80" width="100%" frameBorder="0"></iframe></td>
-            </tr>
-        </table>
+  <div>
+    <button class="cybr-btn" @click="goToPage('home')">
+      Go home<span aria-hidden>_</span>
+      <span aria-hidden class="cybr-btn__glitch">Go home</span>
+      <span aria-hidden class="cybr-btn__tag">#1</span>
+    </button>
+    <div class="center">
+      <h1>Ranking</h1>
+      <table>
+        <tr>
+          <th>Ranking</th>
+          <th>Song Title</th>
+          <th>Song Artist</th>
+          <th>Total Points</th>
+          <th>Total %</th>
+          <th>Spotify Link</th>
+        </tr>
+        <tr v-for="(song, i) in songs" :key="i">
+          <td>#{{ i + 1 }}</td>
+          <td>{{ song.title }}</td>
+          <th>{{ song.artist }}</th>
+          <td>{{ song.totalPoints }}</td>
+          <td>{{ song.percentage }}%</td>
+          <td>
+            <iframe
+              :src="song.spotify"
+              height="80"
+              width="100%"
+              frameBorder="0"
+            ></iframe>
+          </td>
+        </tr>
+      </table>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Rankingpage",
-        data(){
-            return {
-                songs: []
-            }
-        },
-        mounted(){
-            this.fetchSongs();
-        },
-        methods: {
-            goToPage(page) {
-                this.$emit("change-page", page);
-            },
-            fetchSongs() {
-                // Get all songs
-                const url = "http://webservies.be/eurosong/Songs";
+export default {
+  name: "Rankingpage",
+  data() {
+    return {
+      songs: [],
+    };
+  },
+  mounted() {
+    this.fetchSongs();
+  },
+  methods: {
+    goToPage(page) {
+      this.$emit("change-page", page);
+    },
+    fetchSongs() {
+      // Get all songs
+      const url = "http://webservies.be/eurosong/Songs";
 
-                fetch(url)
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((songs) => {
-                        //call fetchVotes method to add totalPoints to songs
-                        this.fetchVotes(songs);
-                        this.fetchArtists(songs);
-                    })
-            },
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((songs) => {
+          //call fetchVotes method to add totalPoints to songs
+          this.fetchVotes(songs);
+          this.fetchArtists(songs);
+        });
+    },
 
-            fetchArtists(songs) {
-                const url = "http://webservies.be/eurosong/Artists";
-                fetch(url)
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((artists) => {
-                        songs.map(song => {
-                            const a = artists.find((artist) => artist.id == song.artist);
-                            song.artist = a.name;
-                            return song;
-                        });
-                        this.songs = songs;
-                    });
-            },
-            fetchVotes(songs) {
-                // Get all votes
-                const url = "http://webservies.be/eurosong/Votes";
+    fetchArtists(songs) {
+      const url = "http://webservies.be/eurosong/Artists";
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((artists) => {
+          songs.map((song) => {
+            const a = artists.find((artist) => artist.id == song.artist);
+            song.artist = a.name;
+            return song;
+          });
+          this.songs = songs;
+        });
+    },
+    fetchVotes(songs) {
+      // Get all votes
+      const url = "http://webservies.be/eurosong/Votes";
 
-                fetch(url)
-                    .then((response) => {
-                        // response is text, so convert to json
-                        return response.json();
-                    })
-                    .then((votes) => {
-                        // loop over array songs with map method and then add totalPoints to songs with the sum of all vote points
-                        let allPoints = 0;
-                        songs.map(song => {
-                            // create totalPoints variable to get the sum of all vote points
-                            song.totalPoints = 0;
-                            //filter all the votes out where the vote songID is the same as the song id, because there are multiple votes for 1 song, then add up all points for 1 song and add it to that song's object in the JSON (totalPoints)
-                            votes.filter((vote) => vote.songID == song.id).forEach(element => {
-                                if (element.songID == song.id) {
-                                    song.totalPoints += element.points
-                                    allPoints += element.points
-                                    }
-                            });
-                            return song;
-                        });
+      fetch(url)
+        .then((response) => {
+          // response is text, so convert to json
+          return response.json();
+        })
+        .then((votes) => {
+          // loop over array songs with map method and then add totalPoints to songs with the sum of all vote points
+          let allPoints = 0;
+          songs.map((song) => {
+            // create totalPoints variable to get the sum of all vote points
+            song.totalPoints = 0;
+            //filter all the votes out where the vote songID is the same as the song id, because there are multiple votes for 1 song, then add up all points for 1 song and add it to that song's object in the JSON (totalPoints)
+            votes
+              .filter((vote) => vote.songID == song.id)
+              .forEach((element) => {
+                if (element.songID == song.id) {
+                  song.totalPoints += element.points;
+                  allPoints += element.points;
+                }
+              });
+            return song;
+          });
 
-                        songs.forEach(song => {
-                            song.percentage = Number((song.totalPoints / allPoints * 100).toFixed(2));
-                            console.log(song.percentage)
-                        })  
-                        // sort songs from best to worst and change data of songs, so everything will get rerenderd;
-                        this.songs = songs.sort((a, b) => b.totalPoints - a.totalPoints );
-                    });
-            }
-        }
-    }
+          songs.forEach((song) => {
+            song.percentage = Number(
+              ((song.totalPoints / allPoints) * 100).toFixed(2)
+            );
+            console.log(song.percentage);
+          });
+          // sort songs from best to worst and change data of songs, so everything will get rerenderd;
+          this.songs = songs.sort((a, b) => b.totalPoints - a.totalPoints);
+        });
+    },
+  },
+};
 </script>
