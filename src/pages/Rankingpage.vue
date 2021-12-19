3f4ec44a753739a062f6b6b98a99c92ea8683dc8
Yannick Van Campenhout <Yannick@cloudclass.be>
2021-12-19 20:51:36 +0100

    Commenting + styling

--- src/pages/Rankingpage.vue
+++ src/pages/Rankingpage.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
+    <!-- Button -->
     <button class="cybr-btn" @click="goToPage('home')">
       Go home<span aria-hidden>_</span>
       <span aria-hidden class="cybr-btn__glitch">Go home</span>
       <span aria-hidden class="cybr-btn__tag">#1</span>
     </button>
+    <!-- FlexBox div  -->
     <div class="center">
+      <!-- H1  -->
       <h1>Ranking</h1>
+      <!-- Table -->
       <table>
         <tr>
+          <!-- Table headers -->
           <th>Ranking</th>
           <th>Song Title</th>
           <th>Song Artist</th>
           <th>Total Points</th>
-          <th>Total %</th>
-          <th>Spotify Link</th>
+          <th>% of Total</th>
+          <th>Spotify Refference</th>
         </tr>
+        <!-- Items themselves -->
+        <!-- For every song in the fetched JSON -->
         <tr v-for="(song, i) in songs" :key="i">
+          <!-- Ranking number -->
           <td>#{{ i + 1 }}</td>
+          <!-- Song details -->
           <td>{{ song.title }}</td>
-          <th>{{ song.artist }}</th>
+          <td>{{ song.artist }}</td>
           <td>{{ song.totalPoints }}</td>
           <td>{{ song.percentage }}%</td>
+          <!-- Fetch spotify song -->
           <td>
             <iframe
               :src="song.spotify"
@@ -41,38 +51,41 @@ export default {
   name: "Rankingpage",
   data() {
     return {
+      //Initialze songs array
       songs: [],
     };
   },
-  mounted() {
-    this.fetchSongs();
-  },
   methods: {
+    //Method to change page to clicked item
     goToPage(page) {
       this.$emit("change-page", page);
     },
     fetchSongs() {
-      // Get all songs
+      // Get all songs from Elke's API
       const url = "http://webservies.be/eurosong/Songs";
 
       fetch(url)
         .then((response) => {
+          //JSON parse the output
           return response.json();
         })
         .then((songs) => {
-          //call fetchVotes method to add totalPoints to songs
+          //Call both methods to get all votes and artists that are needed to be displayed
           this.fetchVotes(songs);
           this.fetchArtists(songs);
         });
     },
 
     fetchArtists(songs) {
+      //Fetch artists from Elke's artists endpoint
       const url = "http://webservies.be/eurosong/Artists";
       fetch(url)
         .then((response) => {
+          //JSON parse output
           return response.json();
         })
         .then((artists) => {
+          //Map each song and get the specified artist from the nested objects
           songs.map((song) => {
             const a = artists.find((artist) => artist.id == song.artist);
             song.artist = a.name;
@@ -82,42 +95,50 @@ export default {
         });
     },
     fetchVotes(songs) {
-      // Get all votes
+      //Fetch artists from Elke's votes endpoint
       const url = "http://webservies.be/eurosong/Votes";
 
       fetch(url)
         .then((response) => {
-          // response is text, so convert to json
+          //JSON parse the output
           return response.json();
         })
         .then((votes) => {
-          // loop over array songs with map method and then add totalPoints to songs with the sum of all vote points
+          //Ininitalize allPoints that is going to show the total amount of points that all songs have together
           let allPoints = 0;
+          //Loop over songs
           songs.map((song) => {
-            // create totalPoints variable to get the sum of all vote points
+            //Initialize new property of class song and set it equal to 0 (this will later on get filled with correct amount of votes)
             song.totalPoints = 0;
-            //filter all the votes out where the vote songID is the same as the song id, because there are multiple votes for 1 song, then add up all points for 1 song and add it to that song's object in the JSON (totalPoints)
+            //Find the correct votes for the corresponding song by filtering
             votes
               .filter((vote) => vote.songID == song.id)
+              //For every vote that was found add the int (of the vote) to the total points and also to the corresponding song
               .forEach((element) => {
                 if (element.songID == song.id) {
                   song.totalPoints += element.points;
                   allPoints += element.points;
                 }
               });
+            //Return song with all new data added
             return song;
           });
 
+          //Calculate for each song the % of votes it holds compared to the previously calculated total
           songs.forEach((song) => {
             song.percentage = Number(
               ((song.totalPoints / allPoints) * 100).toFixed(2)
             );
-            console.log(song.percentage);
           });
-          // sort songs from best to worst and change data of songs, so everything will get rerenderd;
-          this.songs = songs.sort((a, b) => b.totalPoints - a.totalPoints);
+
+          //Get the correct ranking for the songs with all correct data
+          this.songs = songs.sort((y, z) => z.totalPoints - y.totalPoints);
         });
     },
+  },
+  //On load of this page perform the mounted method
+  mounted() {
+    this.fetchSongs();
   },
 };
 </script>
